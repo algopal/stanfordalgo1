260f50af47f68b201f3963e65dedc94ba8880f2a
algopal <30307892+algopal@users.noreply.github.com>
2017-07-19 22:10:11 -0700

    Fixed logic

--- week1/mergesort.py
+++ week1/mergesort.py
@@ -11,9 +11,9 @@ def mergesort(alist):
     return alist
   left_sorted = mergesort(alist[:l/2])
   right_sorted = mergesort(alist[l/2:])
-  
+
   i = j = k = 0
-  
+
   while k < len(alist):
     if i < len(left_sorted) and j < len(right_sorted):
       if left_sorted[i] < right_sorted[j]:
@@ -22,15 +22,15 @@ def mergesort(alist):
       else:
         alist[k] = right_sorted[j]
         j += 1
-    elif i == len(left_sorted) - 1 and j < len(right_sorted):
+    elif i == len(left_sorted) and j < len(right_sorted):
       alist[k] = right_sorted[j]
       j += 1
-    elif j == len(right_sorted) - 1 and i < len(left_sorted):
+    elif j == len(right_sorted) and i < len(left_sorted):
       alist[k] = left_sorted[i]
       i += 1
 
     k += 1
-  
+
   return alist
 
 print mergesort([1,5,2,5,7,32,2,34,2,45,11,6])
