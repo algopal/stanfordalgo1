6426dc3c4b39c50bd80ff2a8fb77f1e246c49583
algopal <30307892+algopal@users.noreply.github.com>
2017-07-20 18:05:54 -0700

    First cut of recursive integer multiplication

--- week1/integer-multiplication.py
+++ week1/integer-multiplication.py
@@ -9,3 +9,27 @@ def arithdigits(num):
     digits.insert(0, num % 10)
     num = num // 10
   return digits
+
+def mult(a,b):
+  # a and b are two lists of digits, same length
+  if len(a) == 1:
+    return a[0]*b[0]
+  
+  l = len(a)
+  mid = l/2
+  aleft = a[:mid]
+  aright = a[mid:]
+  bleft = b[:mid]
+  bright = b[mid:]
+  
+  albl = mult(aleft, bleft)
+  albr = mult(aleft, bright)
+  arbl = mult(aright, bleft)
+  arbr = mult(aright, bright)
+  
+  term1 = 10**(2*(l-1)) * albl
+  term2 = 10**(l-1) * albr
+  term3 = 10**(l-1) * arbl
+  term4 = arbr
+  
+  return term1 + term2 + term3 + term4
