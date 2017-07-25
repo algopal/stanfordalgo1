ac9724f853baee97d27f1f173ec403b36f3480b9
algopal <30307892+algopal@users.noreply.github.com>
2017-07-24 21:16:40 -0700

    Fixed logic

--- week1/integer-multiplication.py
+++ week1/integer-multiplication.py
@@ -1,7 +1,3 @@
-"""
-Implementation of recursive (divide & conquer) integer multiplication
-"""
-
 def arithdigits(num):
   # Returns a list containing the digits in a number, from left to right
   digits = []
@@ -11,29 +7,30 @@ def arithdigits(num):
   return digits
 
 def mult(a,b):
-  # a and b are two lists of digits, same length
   if len(a) == 1 and len(b) == 1:
     return a[0]*b[0]
   if len(a) == 0 or len(b) == 0:
     return 0
-  
-  l = len(a)
-  mid = l/2
-  aleft = a[:mid]
-  aright = a[mid:]
-  bleft = b[:mid]
-  bright = b[mid:]
-  
+
+  alen = len(a)
+  blen = len(b)
+  amid = alen/2
+  bmid = blen/2
+  aleft = a[:amid]
+  aright = a[amid:]
+  bleft = b[:bmid]
+  bright = b[bmid:]
+
   albl = mult(aleft, bleft)
   albr = mult(aleft, bright)
   arbl = mult(aright, bleft)
   arbr = mult(aright, bright)
-  
-  term1 = 10**(2*(l-1)) * albl
-  term2 = 10**(l-1) * albr
-  term3 = 10**(l-1) * arbl
+
+  term1 = 10**(alen-len(aleft)+blen-len(bleft)) * albl
+  term2 = 10**(alen-len(aleft)) * albr
+  term3 = 10**(blen-len(bleft)) * arbl
   term4 = arbr
-  
+
   return term1 + term2 + term3 + term4
 
 num1, num2 = raw_input().split(' ')
